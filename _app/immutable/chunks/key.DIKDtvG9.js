c96737e62954876ae052b8d1c86b42d6e70f8a4f
statox <me@statox.fr>
2025-03-16 19:30:40 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@235eccce17f9fe905afef9d1b8cdd64f1a2dba69 🚀

--- _app/immutable/chunks/key.DIKDtvG9.js
+++
@@ -1,1 +0,0 @@
-import{A as c,Z as i,z as _,B as h,h as r,a as d,W as k,C as u,E as y,U as l}from"./index-client.JlBVMNcv.js";function I(f,n,t){r&&d();var e=f,s=l,a,o=c()?i:_;h(()=>{o(s,s=n())&&(a&&k(a),a=u(()=>t(e)))}),r&&(e=y)}export{I as k};
---
+++ _app/immutable/entry/start.i-tydopk.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/h6nPAO3g.js";export{o as load_css,r as start};
