5e6261c9941a41edf41255179ac935b3a36540c9
statox <me@statox.fr>
2025-09-19 23:30:08 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@e93150655ddd785d6979834fea7a8401dd05e222 🚀

--- _app/immutable/entry/start.Cr2bO9w2.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CpC0DnAw.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.yPQ3Z4xa.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/X6tMhf52.js";export{o as load_css,r as start};
