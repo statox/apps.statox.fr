711e5704c6ee9a4c87aeb5eb01445cfd4a331d58
statox <me@statox.fr>
2025-09-21 21:33:53 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@1c717c59165297e19844c112b01de623c4e265f4 🚀

---
+++ _app/immutable/chunks/DHtTA_T1.js
@@ -0,0 +1,1 @@
+import{s as e}from"./BD0u7zgC.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
