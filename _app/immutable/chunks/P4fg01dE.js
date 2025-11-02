dc05a12bc3fdec52213cb3fd54729fb10b9f1e8a
statox <me@statox.fr>
2025-11-02 22:31:57 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@cc2f9131fffcf59516a50acc97686643fdf4628c 🚀

--- _app/immutable/chunks/P4fg01dE.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CH4jI8fC.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
--- _app/immutable/entry/start.CVH4RLsD.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CH4jI8fC.js";export{o as load_css,r as start};
