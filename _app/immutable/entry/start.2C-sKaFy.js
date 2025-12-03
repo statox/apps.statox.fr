a1eeffa1c0dd3e02b590bc3af6f5027e55392d29
statox <me@statox.fr>
2025-12-03 13:03:13 +0000

    Deploying to gh-pages from @ statox/apps.statox.fr@1f18744ea71284cad62f4357453cbfd2c4fa218a 🚀

--- _app/immutable/entry/start.2C-sKaFy.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/BWpFb33K.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.DbjbPs8g.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/MD5klYRC.js";export{o as load_css,r as start};
